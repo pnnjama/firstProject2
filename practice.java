4964bb8aac3d452582bed6203c6daadf2e5f094e
P Jama <pnnjama@gmail.com>
2022-06-10 15:56:23 +0200

    first commit practice

---
+++ practice.java
@@ -0,0 +1,19 @@
+mqpublic class Practice
+{
+Public static void main  (String[]args)
+{
+//create an object for scanner class
+
+Scanner input = new Scanner (System.in);
+System.out.print("principle");
+doubleprinciple = input.nextDouble();
+System.out.print("time");
+double time = input.nextDouble();
+double interest = (principle*time*7)/100;
+Syatem.out.print("Simple interest : " + interest );
+input.close();
+}
+}
+
+
+
